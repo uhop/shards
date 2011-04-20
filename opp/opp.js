09547b5268dac6d2ed91dc2d0014266ab21907c6
Eugene Lazutkin <eugene@lazutkin.com>
2011-04-19 22:38:53 -0500

    Slight refresh of the operator precedence parser.

--- opp/opp.js
+++ opp/opp.js
@@ -4,160 +4,160 @@ dojo.provide("shards.opp");
 // (c) 2000-2010 Max Motovilov, Eugene Lazutkin, used here under the BSD license
 
 /*
-    State table item is implemented by a naked object:
+	State table item is implemented by a naked object:
 
-    {
-        name:   "+",
-        iPrty:  7000,       // input priority
-        oPrty:  7001,       // output priority
-        before: "operator", // expected state before
-        after:  "operand"   // state after
-        // state-specific data
-    }
+	{
+		name:   "+",
+		iPrty:  7000,       // input priority
+		oPrty:  7001,       // output priority
+		before: "operator", // expected state before
+		after:  "operand"   // state after
+		// state-specific data
+	}
 
-    Priority:
+	Priority:
 
-    iPrty = 0        -- end of statement
-    iPrty < oPrty    -- left associated operator
-    iPrty > oPrty    -- right associated operator
-    iPrty == oPrty   -- non-associated associated operator or brackets
+	iPrty = 0        -- end of statement
+	iPrty < oPrty    -- left associated operator
+	iPrty > oPrty    -- right associated operator
+	iPrty == oPrty   -- non-associated associated operator or brackets
 
-    State:
+	State:
 
-    before == "operand"  && after == "operand"    -- unary prefix operator
-    before == "operator" && after == "operator"   -- unary postfix operator
-    before == "operator" && after == "operand"    -- binary infix operator
-    before == "operand"  && after == "operator"   -- operand
+	before == "operand"  && after == "operand"    -- unary prefix operator
+	before == "operator" && after == "operator"   -- unary postfix operator
+	before == "operator" && after == "operand"    -- binary infix operator
+	before == "operand"  && after == "operator"   -- operand
 
-    Token is implemented by a naked object:
+	Token is implemented by a naked object:
 
-    {
-        name: "+"
-        // token-specific data
-    }
+	{
+		name: "+"
+		// token-specific data
+	}
 
-    Term is implemented by a naked object:
+	Term is implemented by a naked object:
 
-    {
-        token: token,   // see above
-        state: state    // see above
-    }
+	{
+		token: token,   // see above
+		state: state    // see above
+	}
 
-    Scanner produces tokens with the method "getToken()".
+	Scanner produces tokens with the method "getToken()".
 
-    Interpreter consumes terms with the method "putTerm(term)".
+	Interpreter consumes terms with the method "putTerm(term)".
 */
 
 (function(){
-    dojo.declare("shards.opp.Parser", null, {
+	dojo.declare("shards.opp.Parser", null, {
 
-        state:    "supply", // current parser state
-        expected: "",       // expected input
-        term:     null,     // current term
+		state:    "supply",  // current parser state
+		expected: "",        // currently expected input
+		term: null,          // current term
 
-        constructor: function(init, table, brackets){
-            // split the table
-            dojo.forEach(table, function(item){
-                if(!this[item.before]){
-                    this[item.before] = {};
-                }
-                this[item.before][item.name] = item;
-            }, this);
-            //assert(this[init]);
-            this.expected = this.init = init;
-            // split brackets
-            this.brackets = {};
-            for(var i = 0; i + 1 < brackets.length; i += 2){
-                this.brackets[brackets.charAt(i)] = brackets.charAt(i + 1);
-            }
-            // initialize internal variables
-            this.stack = [];
-        },
+		constructor: function(init, table, brackets){
+			// split the table
+			this.tables = {};
+			dojo.forEach(table, function(item){
+				var table = this.tables[item.before];
+				if(!table){
+					table = this.tables[item.before] = {};
+				}
+				table[item.name] = item;
+			}, this);
+			this.expected = this.init = init;
+			// split brackets
+			this.brackets = {};
+			for(var i = 0; i + 1 < brackets.length; i += 2){
+				this.brackets[brackets.charAt(i)] = brackets.charAt(i + 1);
+			}
+			// initialize internal variables
+			this.stack = [];
+		},
 
-        reset: function(){
-            this.stack = [];
-            this.state = "supply";
-            this.expected = this.init;
-        },
+		reset: function(){
+			this.stack = [];
+			this.state = "supply";
+			this.expected = this.init;
+		},
 
-        supply: function(token){
-            //assert(this.state == "supply");
-            var newState = this[this.expected][token.name];
-            if(!newState){
-                throw Error("Unknown input symbol: " + token.name);
-            }
-            this.term = {token: token, state: newState};
-            this._decide();
-        },
+		supply: function(token){
+			//assert(this.state == "supply");
+			var newState = this.tables[this.expected][token.name];
+			if(!newState){
+				throw Error("Unknown input symbol: " + token.name + ", " + this.expected + " was expected");
+			}
+			this.term = {token: token, state: newState};
+			this._decide();
+		},
 
-        consume: function(){
-            if(this.state == "eos"){
-                this.state = "done";
-                return this.term;
-            }
+		consume: function(){
+			if(this.state == "eos"){
+				this.state = "done";
+				return this.term;
+			}
 
-            //assert((this.state in consumeReadyState) && this.stack.length);
-            var t = this.stack.pop();
+			//assert((this.state in lib.op.Parser.consumeReadyState) && this.stack.length);
+			var t = this.stack.pop();
 
-            if(this.state == "consume"){
-                this._decide();
-            }else{
-                //assert(this.state == "bracket");
-                this.stack.push(this.term);
-                this.state = "supply";
-            }
+			if(this.state == "consume"){
+				this._decide();
+			}else{
+				//assert(this.state == "bracket");
+				this.stack.push(this.term);
+				this.state = "supply";
+			}
 
-            return t;
-        },
+			return t;
+		},
 
-        _decide: function(){
-            var iPrty = this.term.state.iPrty,
-                oPrty = this.stack.length ? this.stack[this.stack.length - 1].state.oPrty : 0;
+		_decide: function(){
+			var iPrty = this.term.state.iPrty,
+				oPrty = this.stack.length ? this.stack[this.stack.length - 1].state.oPrty : 0;
 
-            if(iPrty > oPrty){
-                this.expected = this.term.state.after;
-                this.stack.push(this.term);
-                this.state = "supply";
-            }else if(iPrty < oPrty){
-                this.state = this.stack.length ? "consume" : "eos";
-            }else{
-                if(this.brackets[this.stack[this.stack.length - 1].token.name] == this.term.token.name){
-                    this.state = "bracket";
-                }else{
-                    throw Error("Unbalanced brackets or operator is not associative");
-                }
-            }
-        }
-    });
+			if(iPrty > oPrty){
+				this.expected = this.term.state.after;
+				this.stack.push(this.term);
+				this.state = "supply";
+			}else if(iPrty < oPrty){
+				this.state = this.stack.length ? "consume" : "eos";
+			}else{
+				if(this.brackets[this.stack[this.stack.length - 1].token.name] == this.term.token.name){
+					this.state = "bracket";
+				}else{
+					throw Error("Unbalanced brackets or operator is not associative");
+				}
+			}
+		}
+	});
 
-    var consumeReadyState = {consume: 1, bracket: 1, eos: 1};
+	var consumeReadyState = {consume: 1, bracket: 1, eos: 1};
 
-    // "lazy" operations
+	// "lazy" operations
 
-    // "pull" mode of operations
-    shards.opp.getNextTerm = function getNextTerm(parser, scanner){
-        while(parser.state == "supply"){
-            parser.supply(scanner.getToken());
-        }
-        return parser.state == "done" ? null : parser.consume();
-    };
+	// "pull" mode of operations
+	shards.opp.getTerm = function getNextTerm(parser, scanner){
+		while(parser.state == "supply"){
+			parser.supply(scanner.getToken());
+		}
+		return parser.state == "done" ? null : parser.consume();
+	};
 
-    // "push" mode of operations
-    shards.opp.putNextToken = function putNextToken(parser, token, interpreter){
-        if(parser.state == "done"){
-            return false;
-        }
-        //assert(parser.state == "supply");
-        parser.supply(token);
-        while(parser.state in consumeReadyState){
-            interpreter.putTerm(parser.consume());
-        }
-        return parser.state != "done";
-    };
+	// "push" mode of operations
+	shards.opp.putToken = function putNextToken(parser, token, interpreter){
+		if(parser.state == "done"){
+			return false;
+		}
+		//assert(parser.state == "supply");
+		parser.supply(token);
+		while(parser.state in consumeReadyState){
+			interpreter.putTerm(parser.consume());
+		}
+		return parser.state != "done";
+	};
 
-    // parsing complete statement
-    shards.opp.parse = function parse(parser, scanner, interpreter){
-        var putNextToken = shards.opp.putNextToken;
-        while(putNextToken(parser, scanner.getToken(), interpreter)){}
-    }
+	// parsing complete statement
+	shards.opp.parse = function parse(parser, scanner, interpreter){
+		while(shards.opp.putToken(parser, scanner.getToken(), interpreter)){}
+	}
 })();
